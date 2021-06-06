88eccf193370b0800945ff66525c88e147ec28d8
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-06-06 12:53:19 +0530

    front-matter

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,11 +1,31 @@
-import Layout from "../../components/Layout"
-
-
+import fs from 'fs';
+import path from 'path';
+import Layout from '../../components/Layout';
+import matter from 'gray-matter'
 
 export default function BlogPage() {
-    return (
-        <Layout title='Blog' >
-            Blog
-        </Layout>
-    )
+	return <Layout title='Blog'>Blog</Layout>;
 }
+
+export const getStaticProps = async () => {
+	const files = fs.readdirSync(path.join('posts'));
+
+	const posts = files.map((filename) => {
+		const slug = filename.replace('.md', '');
+
+        const markdownWithMeta = fs.readFileSync(path.join('posts',filename),'utf-8')
+
+        const {data:frontmatter}=  matter(markdownWithMeta);
+
+		return {
+			slug,
+            frontmatter
+		};
+	});
+
+   
+
+	return {
+		props: {},
+	};
+};
