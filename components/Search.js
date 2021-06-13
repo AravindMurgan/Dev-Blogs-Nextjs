c132cc1154f67180cd26ba36eadd5bd4bbd1419a
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-06-13 17:28:25 +0530

    search-functionality

--- components/Search.js
+++ components/Search.js
@@ -1,10 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {FaSearch} from 'react-icons/fa'
 
 
 export default function Search() {
 	const [searchTerm, setSearchTerm] = useState('');
 	const [searchResults, setSearchResults] = useState([]);
+
+    useEffect(()=>{
+
+        const getResults = async () =>{
+
+            const res = await fetch(`/api/search?q=${searchTerm}`)
+            const {results} = await res.json()
+           setSearchResults(results)
+        }
+
+        getResults()
+
+    },[searchTerm])
 
 	return (
 		<div className='relative bg-gray-600 p-4'>
