d55be3c83a5480e8cd5b33ca4671774e698c88ab
Aravind Murugan <mi.arvindmurugan@gmail.com>
2021-06-13 14:56:29 +0530

    searchComponent

---
+++ components/Search.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react"
+import {FaSearch} from 'react-icons/fa'
+
+export default function Search() {
+    
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
